3953982de2f4f96565a0421917572c1ff946119d
aleksandrandreichenko <andreichenko@web.de>
2021-11-10 22:58:22 +0300

    rename cluster and reuse modules + fix

--- cluster.tf
+++ cluster.tf
@@ -1,29 +1,29 @@
 module "network" {
   source = "git::https://github.com/Andreichenko/terraform-gcp-vpc.git//vpc?ref=gke-node-pool-v2.0.1"
-  network_name     = "kube"
-  subnetwork_name  = "kube-subnet"
+  net_name     = "kube"
+  subnet_name  = "kube-subnet"
   region           = "us-central1"
   enable_flow_logs = "false"
   // subnetwork primary and secondary CIDRS for IP aliasing
-  subnetwork_range    = "10.240.0.0/16"
-  subnetwork_pods     = "10.241.0.0/16"
-  subnetwork_services = "10.242.0.0/16"
+  subnet_range    = "10.240.0.0/16"
+  subnet_pods     = "10.241.0.0/16"
+  subnet_services = "10.242.0.0/16"
 }
 
 module "cluster" {
-  source                           = "git::https://github.com/Andreichenko/terraform-gke.git//native-vpc?ref=gke-node-pool-v2.0.1"
+  source                           = "git::https://github.com/Andreichenko/terraform-gcp-vpc.git//native-vpc?ref=gke-node-pool-v2.0.1"
   region                           = "us-central1"
   name                             = "gke-cluster"
   project                          = "terraform-module-cluster"
   network_name                     = "kube"
   nodes_subnetwork_name            = module.network.subnet
-  kubernetes_version               = "1.16.10-gke.8"
+  kubernetes_version               = "1.20.10-gke.1600"
   pods_secondary_ip_range_name     = module.network.gke_pod
   services_secondary_ip_range_name = module.network.gke_service
 }
 
 module "node_pool" {
-  source             = "git::https://github.com/Andreichenko/terraform-gke//node_pool?ref=gke-node-pool-v2.0.1"
+  source             = "git::https://github.com/Andreichenko/terraform-gcp-vpc.git//node-pools?ref=gke-node-pool-v2.0.1"
   name               = "gke-cluster-node-pool"
   region             = module.cluster.region
   gke_cluster_name   = module.cluster.name
